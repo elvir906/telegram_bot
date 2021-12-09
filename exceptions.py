6f58567b8c873b56f2cd19abf36072987cb83314
Your Nameelvir906 <himynameiselvir@gmail.com>
2021-12-09 22:50:53 +0500

    bot

---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class responseNotInListFormat(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -1,17 +1,22 @@
+import os
+
 import requests
 import telegram
 import telegram.ext
 import time
+import exceptions
+import hw_status
 
-from pprint import pprint
+#from pprint import pprint
 from dotenv import load_dotenv
+#from .exceptions import responseNotInListFormat
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -26,36 +31,37 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    api_answer = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return api_answer.json()
 
 
 def check_response(response):
-
-    ...
+    try:
+        homework = response.get('homeworks')
+    except:
+        raise exceptions.responseNotInListFormat('!!!!!!!сюда надо логгинг')
+    return homework
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status != hw_status.status:
+        verdict = HOMEWORK_STATUSES['homework_status']
+        hw_status.status = homework_status
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    else:
+        return 'Статус не изменился'
 
 
 def check_tokens():
-    ...
+    if 
 
 
 def main():
