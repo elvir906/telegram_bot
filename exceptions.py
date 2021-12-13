5b3cbbb6c3ef99858e9072bd9e1305f55956639c
Your Nameelvir906 <himynameiselvir@gmail.com>
2021-12-13 22:37:53 +0500

    bot

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,26 @@
 class responseNotInListFormat(Exception):
     pass
 
+
 class someTokensAreNotAvailable(Exception):
     pass
+
 
 class endpointIsUnavailable(Exception):
     pass
 
+
 class expectedKeysMissing(Exception):
     pass
 
+
 class UnexpectedStaus(Exception):
+    pass
+
+
+class requestCausedError500(Exception):
+    pass
+
+
+class dictionaryIsEempty(Exception):
     pass
--- testir.py
+++ testir.py
@@ -47,70 +47,28 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Здесь мы получаем ответ от api и преобразовываем его в json-формат."""
-    timestamp = current_timestamp or int(time.time()) - 3888000
+    timestamp = current_timestamp or int(time.time() - 3888000)
     params = {'from_date': timestamp}
     api_answer = requests.get(ENDPOINT, headers=HEADERS, params=params)
     if api_answer.status_code != 200:
         raise exceptions.requestCausedError500
     return api_answer.json()
 
-
 def check_response(response):
     """Получаем домашние работы с ответа api."""
     hw = response.get('homeworks')
-    if hw[0] != []:
-        homework = hw[0]
-        return homework
-    else:
-        raise exceptions.dictionaryIsEempty
-
+    homework = hw[0]
+    return homework
 
 def parse_status(homework):
     """Тут проверяется и определяется статус д/з."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    if homework_status != hw_status.status:
-        if (homework_status in HOMEWORK_STATUSES) and (homework_name):
-            verdict = HOMEWORK_STATUSES[homework_status]
-            hw_status.status = homework_status
-            text = ('Изменился статус проверки '
-                    f'работы "{homework_name}". {verdict}')
-            return text
-        else:
-            raise exceptions.UnexpectedStaus
-    else:
-        logging.debug(
-            f'Статус проверки {homework_status} домашней '
-            f'работы {homework_name} не изменился'
-        )
+    print(homework_name, homework_status)
 
+a = get_api_answer(int(time.time()) - 3888000)
+print(a)
+b = check_response(a)
+print(b)
+parse_status(b)
 
-def check_tokens():
-    """Проверка токенов."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    else:
-        return False
-
-
-def main():
-    """Основная логика работы бота."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()) - 3888000
-    while check_tokens is True:
-        try:
-            response = get_api_answer(current_timestamp)
-            homework = check_response(response)
-            message = parse_status(homework)
-            send_message(bot, message)
-            current_timestamp = int(time.time())
-            time.sleep(RETRY_TIME)
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
-            logging.error(message)
-            time.sleep(RETRY_TIME)
-
-
-if __name__ == '__main__':
-    main()
